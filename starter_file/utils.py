ed2388c49400bf0dcc700bb8ab0b1589bf3a12bc
YaraSco <canina.assya@gmail.com>
2022-06-26 13:05:14 -0400

    Update utils.py

--- starter_file/utils.py
+++ starter_file/utils.py
@@ -1,78 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-
-
-import subprocess
-import pandas as pd
-from git import Repo
-from sys import platform
-from pkgutil import iter_modules
-
-
-def git_repo_path():
-    """Return the path of the forecasting repo"""
-
-    repo = Repo(search_parent_directories=True)
-    return repo.working_dir
-
-
-def module_exists(module_name):
-    """Check if a package is installed.
-
-    Args:
-        module_name (str): name of the package/module
-    
-    Returns:
-        bool: True if module exists; otherwise False
-    """
-
-    return module_name in (name for loader, name, ispkg in iter_modules())
-
-
-def system_type():
-    """Return type of the current operating system"""
-
-    if platform == "linux" or platform == "linux2":
-        system_type = "linux"
-    elif platform == "darwin":
-        system_type = "mac"
-    elif platform == "win32":
-        system_type = "win"
-    return system_type
-
-
-def module_path(env_name, module_name):
-    """Return the path of a module in a conda environment.
-
-    Args:
-        env_name (str): name of the conda environment
-        module_name (str): name of the package/module	
-
-    Returns:
-        str: path of the package/module
-    """
-
-    system = system_type()
-    if system == "win":
-        command = "where " + module_name
-    else:
-        command = "which " + module_name
-    all_paths = subprocess.check_output(command, shell=True)
-    all_paths = all_paths.decode("utf-8").split("\n")
-    all_paths = [path for path in all_paths if env_name in path]
-    module_path = ""
-    if all_paths:
-        module_path = all_paths[0]
-    if system == "win":
-        # Remove additional char \r
-        module_path = module_path[:-1]
-    return module_path
-
-
 # Source repo:
 # https://github.com/Azure/MachineLearningNotebooks/blob/master/\
 # how-to-use-azureml/automated-machine-learning/forecasting-orange-\
 # juice-sales/forecasting_helper.py
+
+import pandas as pd
+
 def align_outputs(
     y_predicted,
     X_trans,
@@ -80,7 +14,7 @@ def align_outputs(
     y_test,
     target_column_name,
     predicted_column_name="predicted",
-    horizon_colname="horizon_origin",
+    horizon_colname="horizon_origin"
 ):
     """
     Demonstrates how to get the output aligned to the inputs
