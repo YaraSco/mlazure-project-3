a741cb428bc2272ebe6ea8b484ff977035f4bfbf
YaraSco <53176263+YaraSco@users.noreply.github.com>
2022-07-12 17:43:09 -0400

    Update train.py

--- starter_file/hyperdrive/train.py
+++ starter_file/hyperdrive/train.py
@@ -22,7 +22,7 @@ import lightgbm as lgb
 
 from azureml.core import Run, Dataset
 from sklearn.model_selection import train_test_split
-from feature_utils import week_of_month, df_from_cartesian_product, combine_features
+from hyperdrive.feature_utils import week_of_month, df_from_cartesian_product, combine_features
 
 
 FIRST_WEEK = 40
